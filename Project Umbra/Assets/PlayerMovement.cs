50eb9072d8a5f38e6deaf79e4dbbd9528c7cff90
Tomasz Kraszewski <Snakerrek@gmail.com>
2020-10-04 15:57:48 +0200

    BasicPlayerMovement

---
+++ Project Umbra/Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    [SerializeField] float maxSpeed = 2.0f;
+    [SerializeField] float force = 50.0f;
+    void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (Input.GetKey("up") || Input.GetKey("w"))
+            rb2D.AddForce(new Vector2(0, force * Time.deltaTime));
+        if (Input.GetKey("down") || Input.GetKey("s"))
+            rb2D.AddForce(new Vector2(0, -force * Time.deltaTime));
+        if (Input.GetKey("left") || Input.GetKey("a"))
+            rb2D.AddForce(new Vector2(-force * Time.deltaTime, 0));
+        if (Input.GetKey("right") || Input.GetKey("d"))
+            rb2D.AddForce(new Vector2(force * Time.deltaTime, 0));
+
+        rb2D.velocity = (Vector3.ClampMagnitude(rb2D.velocity, maxSpeed));
+    }
+
+}
