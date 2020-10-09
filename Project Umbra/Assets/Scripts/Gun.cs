09e3b8d82503373a3100b13dcc36e2e5824d1657
Tomasz Kraszewski <Snakerrek@gmail.com>
2020-10-09 15:23:26 +0200

    Player gun

---
+++ Project Umbra/Assets/Scripts/Gun.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] GameObject barrel;
+    [SerializeField] GameObject projectilePrefab;
+    void Update()
+    {
+        FaceMouse();
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+
+    void FaceMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(
+            mousePosition.x - transform.position.x,
+            mousePosition.y - transform.position.y
+            );
+
+        transform.up = direction;
+    }
+    void Shoot()
+    {
+        Vector2 position = new Vector2(barrel.transform.position.x, barrel.transform.position.y);
+        Instantiate(projectilePrefab, position, transform.rotation);
+
+    }
+}
