38d57ddb103eea540958da1a3d4054d4bb1bf5dc
Tomasz Kraszewski <Snakerrek@gmail.com>
2020-10-09 12:03:04 +0200

    Engine particle fix

--- Project Umbra/Assets/Scripts/PlayerMovement.cs
+++ Project Umbra/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,12 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb2D;
     [SerializeField] float maxSpeed = 2.0f;
     [SerializeField] float force = 50.0f;
+
+    [Header("Engines")]
+    [SerializeField] ParticleSystem leftEngineParticles;
+    [SerializeField] ParticleSystem rightEngineParticles;
+    [SerializeField] ParticleSystem upEngineParticles;
+    [SerializeField] ParticleSystem downEngineParticles;
     void Start()
     {
         rb2D = gameObject.GetComponent<Rigidbody2D>();
@@ -21,19 +27,33 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey("up") || Input.GetKey("w"))
         {
             rb2D.AddForce(new Vector2(0, force * Time.deltaTime));
+            downEngineParticles.Play();
         }
+        else
+            downEngineParticles.Stop();
+
         if (Input.GetKey("down") || Input.GetKey("s"))
         {
             rb2D.AddForce(new Vector2(0, -force * Time.deltaTime));
+            upEngineParticles.Play();
         }
+        else
+            upEngineParticles.Stop();
+
         if (Input.GetKey("left") || Input.GetKey("a"))
         {
             rb2D.AddForce(new Vector2(-force * Time.deltaTime, 0));
+            rightEngineParticles.Play();
         }
+        else
+            rightEngineParticles.Stop();
         if (Input.GetKey("right") || Input.GetKey("d"))
         {
             rb2D.AddForce(new Vector2(force * Time.deltaTime, 0));
+            leftEngineParticles.Play();
         }
+        else
+            leftEngineParticles.Stop();
 
         rb2D.velocity = (Vector3.ClampMagnitude(rb2D.velocity, maxSpeed));
     }
