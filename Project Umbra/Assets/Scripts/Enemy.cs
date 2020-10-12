416b4397f35e0211effdc8276844ccbdc5bd41cc
Tomasz Kraszewski <Snakerrek@gmail.com>
2020-10-12 17:27:42 +0200

    Basic enemy movement

--- Project Umbra/Assets/Scripts/Enemy.cs
+++ Project Umbra/Assets/Scripts/Enemy.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Movement Settings")]
 
-    // Update is called once per frame
+    [SerializeField] float enemyOffset = 4.0f;
+    [SerializeField] float enemySpeed = 3.0f;
+
+    Player player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
     void Update()
     {
-        
+        if (CheckDistanceFromPlayer() > enemyOffset)
+            FollowPlayer();
+        FacePlayer();
+    }
+
+    void FollowPlayer()
+    {
+        Vector2 target = player.transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, target, enemySpeed * Time.deltaTime);
+    }
+
+    float CheckDistanceFromPlayer()
+    {
+        float distance;
+        Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y);
+        Vector2 enemyPos = new Vector2(transform.position.x, transform.position.y);
+
+        distance = Mathf.Sqrt(Mathf.Pow(enemyPos.x - playerPos.x, 2) + Mathf.Pow(enemyPos.y - playerPos.y, 2));
+        return distance;
+    }
+    void FacePlayer()
+    {
+        Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y);
+
+        Vector2 direction = new Vector2(
+            playerPos.x - transform.position.x,
+            playerPos.y - transform.position.y
+            );
+
+        transform.up = direction;
     }
 }
