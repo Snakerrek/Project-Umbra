7b896046f93e713e587db27deecd2cb35dc684a2
Tomasz Kraszewski <Snakerrek@gmail.com>
2020-10-12 18:00:48 +0200

    Basic enemy shooting

--- Project Umbra/Assets/Scripts/Enemy.cs
+++ Project Umbra/Assets/Scripts/Enemy.cs
@@ -9,33 +9,58 @@ public class Enemy : MonoBehaviour
     [SerializeField] float enemyOffset = 4.0f;
     [SerializeField] float enemySpeed = 3.0f;
 
-    Player player;
+    [Header("Attack Settings")]
 
+    [SerializeField] float enemyShootDistance = 6.0f;
+    [SerializeField] float enemyShootRatio = 2.0f;
+    [SerializeField] GameObject projectilePrefab = null;
+
+    Player player;
+    bool canShoot = true;
     private void Start()
     {
         player = FindObjectOfType<Player>();
     }
-    void Update()
+    void FixedUpdate()
     {
-        if (CheckDistanceFromPlayer() > enemyOffset)
-            FollowPlayer();
+        if(IsDistanceFromPlayerSmallerThan(enemyShootDistance) && canShoot)
+            StartCoroutine(Shoot());
+
+        FollowPlayer();
         FacePlayer();
     }
 
+    IEnumerator Shoot()
+    {
+        canShoot = false;
+        Vector2 position = new Vector2(transform.position.x, transform.position.y);
+        Instantiate(projectilePrefab, position, transform.rotation);
+        yield return new WaitForSeconds(enemyShootRatio);
+        canShoot = true;
+    }
     void FollowPlayer()
     {
-        Vector2 target = player.transform.position;
-        transform.position = Vector2.MoveTowards(transform.position, target, enemySpeed * Time.deltaTime);
+        if(!IsDistanceFromPlayerSmallerThan(enemyOffset * 2))
+        {
+            Vector2 target = player.transform.position;
+            transform.position = Vector2.MoveTowards(transform.position, target, enemySpeed * 2 * Time.deltaTime);
+        }
+        else if (!IsDistanceFromPlayerSmallerThan(enemyOffset))
+        {
+            Vector2 target = player.transform.position;
+            transform.position = Vector2.MoveTowards(transform.position, target, enemySpeed * Time.deltaTime);
+        }
+
     }
 
-    float CheckDistanceFromPlayer()
+    bool IsDistanceFromPlayerSmallerThan( float targetDistance)
     {
         float distance;
         Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y);
         Vector2 enemyPos = new Vector2(transform.position.x, transform.position.y);
 
         distance = Mathf.Sqrt(Mathf.Pow(enemyPos.x - playerPos.x, 2) + Mathf.Pow(enemyPos.y - playerPos.y, 2));
-        return distance;
+        return distance < targetDistance;
     }
     void FacePlayer()
     {
