9c6ee0752c8201c59d259db124a5207431edbab0
Tomasz Kraszewski <Snakerrek@gmail.com>
2020-10-15 20:05:31 +0200

    Meteorites

---
+++ Project Umbra/Assets/Meteorite.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteorite : MonoBehaviour
+{
+    [SerializeField] float speed = 0.1f;
+    [SerializeField] float lifespan = 30.0f;
+
+    private Rigidbody2D rb;
+    Player player;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<Player>();
+        FacePlayer();
+    }
+
+    void Update()
+    {
+        rb.AddForce(rb.transform.up * speed * Time.deltaTime);
+        Destroy(gameObject, lifespan);
+    }
+
+    void FacePlayer()
+    {
+        Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y);
+
+        Vector2 direction = new Vector2(
+            playerPos.x - transform.position.x,
+            playerPos.y - transform.position.y
+            );
+
+        transform.up = direction;
+    }
+}
---
+++ Project Umbra/Assets/MeteoritesSpawner.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteoritesSpawner : MonoBehaviour
+{
+    [Header("Spawner tweaks")]
+    [SerializeField] float timeBetweenMeteoritesSpawn = 300.0f;
+
+    [SerializeField] GameObject[] meteoritesPrefabs = null;
+    Player player;
+
+    Vector2[] vectors =
+    {
+        new Vector2(0, 32.0f),
+        new Vector2(0, -32.0f),
+        new Vector2(32.0f, 0),
+        new Vector2(-32.0f, 0),
+        new Vector2(-32.0f, -32.0f),
+        new Vector2(32.0f, 32.0f),
+        new Vector2(-32.0f, 32.0f),
+        new Vector2(32.0f,-32.0f)
+    };
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+        InvokeRepeating("SpawnGroupOfMeteorites", timeBetweenMeteoritesSpawn, timeBetweenMeteoritesSpawn);
+    }
+    void SpawnGroupOfMeteorites()
+    {
+        int posVarriantIndex = Mathf.FloorToInt(Random.Range(0, vectors.Length));
+        Vector2 position = new Vector2(player.transform.position.x, player.transform.position.y) + vectors[posVarriantIndex];
+
+        int caseSwitch = Random.Range(1, 4);
+        // Creating shapes from the metheorites groups
+        switch(caseSwitch)
+        {
+            case 1:
+                for (int i = 0; i < 5; i++)        //  Triangle    ****
+                    for(int j = i; j < 5; j++)     //              ***
+                        Spawn(position, i, j);     //              *
+                break;
+            case 2:
+                for (int i = 0; i < 5; i++)       //  Triangle       *
+                    for (int j = i; j < 5; j++)   //               ***
+                        Spawn(position, j, i);    //              ****
+                break;
+            case 3:
+                for (int i = 0; i < 5; i++)       //  Square       
+                    for (int j = 0; j < 5; j++)   //      ***
+                        Spawn(position, i, j);    //      ***
+                break;
+
+        }
+
+
+    }
+
+    void Spawn(Vector2 position, float x, float y)
+    {
+        int prefabIndex = Random.Range(0, meteoritesPrefabs.Length);
+        var meteorite = Instantiate(meteoritesPrefabs[prefabIndex], position + new Vector2(x, y), Quaternion.identity);
+        meteorite.transform.parent = gameObject.transform;
+    }
+}
