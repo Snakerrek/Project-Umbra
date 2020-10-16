d49766c0e8b73b6841755f8e01d7d1cbfc643e6a
Tomasz Kraszewski <Snakerrek@gmail.com>
2020-10-16 13:16:52 +0200

    Adding currency

--- Project Umbra/Assets/Scripts/GameController.cs
+++ Project Umbra/Assets/Scripts/GameController.cs
@@ -1,27 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField] private int coins = 0;
+    [SerializeField] GameObject coinsText = null;
+
+    TextMeshProUGUI coinTMP;
 
     const float dataSaveTimePeriod = 15.0f;
+    private int coins = 0;
 
     private void Awake()
     {
         LoadData();
+        UpdateCoinsText();
     }
     private void Start()
     {
         InvokeRepeating("SaveData", dataSaveTimePeriod, dataSaveTimePeriod); // Saving game data every *dataSaveTimePeriod* seconds
     }
 
+    private void Update()
+    {
+        // Testing coins
+        if(Input.GetKeyDown("o"))
+        {
+            AddCoins(10);
+        }
+        if(Input.GetKeyDown("p"))
+        {
+            subtractCoins(10);
+        }
+    }
 
     #region Coins Methods
 
     public int GetCoins() {return coins;}
-    public void AddCoins(int amount) {coins += amount;}
+    public void AddCoins(int amount) 
+    {
+        coins += amount;
+        Debug.Log(amount + " coins has been added.");
+        UpdateCoinsText();
+    }
+    public void subtractCoins(int amount)
+    {
+        coins -= amount;
+        Debug.Log(amount + " coins has been substracted");
+        UpdateCoinsText();
+    }
+    void UpdateCoinsText() 
+    {
+        coinTMP = coinsText.GetComponent<TextMeshProUGUI>();
+        coinTMP.text = coins.ToString(); 
+    }
 
     #endregion
 
