c867408750b2c8d920a372a5bf9ee81cbcc2b50c
Tomasz Kraszewski <Snakerrek@gmail.com>
2020-10-17 16:29:33 +0200

    Destroying meteorites fix

--- Project Umbra/Assets/Scripts/Meteorite.cs
+++ Project Umbra/Assets/Scripts/Meteorite.cs
@@ -9,17 +9,22 @@ public class Meteorite : MonoBehaviour
 
     private Rigidbody2D rb;
     Player player;
+    float timer;
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         player = FindObjectOfType<Player>();
         FacePlayer();
+        timer = 0.0f;
     }
 
     void Update()
     {
+        timer += Time.deltaTime;
         rb.AddForce(rb.transform.up * speed * Time.deltaTime);
-        Destroy(gameObject, lifespan);
+
+        if(timer > lifespan && DistanceFromPlayer() > 16)
+            Destroy(gameObject);
     }
 
     void FacePlayer()
@@ -32,5 +37,15 @@ public class Meteorite : MonoBehaviour
             );
 
         transform.up = direction;
+    }
+
+    float DistanceFromPlayer()
+    {
+        float distance;
+        Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y);
+        Vector2 meteoritePos = new Vector2(transform.position.x, transform.position.y);
+
+        distance = Mathf.Sqrt(Mathf.Pow(meteoritePos.x - playerPos.x, 2) + Mathf.Pow(meteoritePos.y - playerPos.y, 2));
+        return distance;
     }
 }
